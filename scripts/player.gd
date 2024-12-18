1e1e6506726f76d90fac141ef2c5ea17316afdef
Tanner <tannerelephant@gmail.com>
2024-12-17 20:40:26 -0800

    add basic jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 10000
+@export var jump_speed : float = 500
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
@@ -11,4 +12,10 @@ func _process(delta: float) -> void:
 		velocity.x = move_speed * delta
 	else:
 		velocity.x = 0
+	
+	velocity.y += get_gravity().y * delta
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y = -jump_speed
+		
 	move_and_slide()
