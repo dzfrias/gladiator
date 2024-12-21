507635c3061f2ec81a36e63be70aa1100e0ccd44
Tanner <tannerelephant@gmail.com>
2024-12-20 17:45:18 -0800

    add raycast for shooting

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var move_speed : float = 10000
 @export var jump_speed : float = 500
 
+@export var weapon_range : float = 2000
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
 		velocity.x = 0
@@ -19,3 +21,19 @@ func _process(delta: float) -> void:
 		velocity.y = -jump_speed
 		
 	move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fire"):
+		var mouse_position = get_global_mouse_position()
+
+		var space_state = get_world_2d().direct_space_state
+		var direction = (mouse_position - global_position).normalized()
+		var query = PhysicsRayQueryParameters2D.create(position, position + (direction * weapon_range))
+		query.exclude = [self]
+		var result = space_state.intersect_ray(query)
+		print("Hit: " + str(result))
+		
+func _physics_process(delta):
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), Vector2(50, 100))
+	var result = space_state.intersect_ray(query)
