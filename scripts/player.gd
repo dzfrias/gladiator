b0f10f77651cb8b575c30bb5d39d7bf826e2e2ad
Tanner <tannerelephant@gmail.com>
2024-12-21 07:49:16 -0800

    separate weapon from player into separate script

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,7 @@ extends CharacterBody2D
 @export var move_speed : float = 10000
 @export var jump_speed : float = 500
 
-@export var weapon_range : float = 2000
-@export var damage : float = 5
-@export var bullet_path_scene : PackedScene
+@export var weapon : Weapon
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
@@ -23,26 +21,7 @@ func _process(delta: float) -> void:
 		velocity.y = -jump_speed
 		
 	move_and_slide()
-	
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fire"):
-		var mouse_position = get_global_mouse_position()
-
-		var space_state = get_world_2d().direct_space_state
-		var direction = (mouse_position - global_position).normalized()
-		var query = PhysicsRayQueryParameters2D.create(position, position + (direction * weapon_range))
-		query.exclude = [self]
-		var result = space_state.intersect_ray(query)
-		var bullet_path = bullet_path_scene.instantiate() as Line2D
-		get_tree().root.add_child(bullet_path)
-		bullet_path.add_point(query.from, 0)
-		bullet_path.add_point(query.to, 1)
-		
-		if result.get("collider") != null:
-			var hit_collider = result.get("collider") as CollisionObject2D
-			for child in hit_collider.get_children():
-				if child is Health:
-					var hit_object_health = child as Health
-					hit_object_health.take_damage(damage)
-			
-		print("Hit: " + str(result))
+		weapon.fire(position)
---
+++ scripts/weapon.gd
@@ -0,0 +1,29 @@
+class_name Weapon
+
+extends Node2D
+
+@export var weapon_range : float = 2000
+@export var damage : float = 5
+@export var bullet_path_scene : PackedScene
+
+func fire(pos: Vector2):
+	var mouse_position = get_global_mouse_position()
+
+	var space_state = get_world_2d().direct_space_state
+	var direction = (mouse_position - pos).normalized()
+	var query = PhysicsRayQueryParameters2D.create(pos, pos + (direction * weapon_range))
+	query.exclude = [self]
+	var result = space_state.intersect_ray(query)
+	var bullet_path = bullet_path_scene.instantiate() as Line2D
+	get_tree().root.add_child(bullet_path)
+	bullet_path.add_point(query.from, 0)
+	bullet_path.add_point(query.to, 1)
+	
+	if result.get("collider") != null:
+		var hit_collider = result.get("collider") as CollisionObject2D
+		for child in hit_collider.get_children():
+			if child is Health:
+				var hit_object_health = child as Health
+				hit_object_health.take_damage(damage)
+		
+	print("Hit: " + str(result))
