cda55dda21b8d7cb0910f2ba9528ee8b5e5879e1
Diego Frias <styx5242@gmail.com>
2024-12-21 20:40:56 -0800

    Add player roll
    Activated with the E button for now.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,24 +2,45 @@ class_name Player
 
 extends CharacterBody2D
 
-@export var move_speed: float = 10000
+@export var move_speed: float = 400
 @export var jump_speed: float = 500
+@export var roll_speed: float = 800
+
+var _state := State.CONTROL
+var _direction: float = 1
+@onready var _init_layer = collision_layer
+
+enum State {
+	CONTROL,
+	ROLL,
+}
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
-		velocity.x = 0
-	elif Input.is_action_pressed("left"):
-		velocity.x = -move_speed
-	elif Input.is_action_pressed("right"):
-		velocity.x = move_speed
-	else:
-		velocity.x = 0
-
 	if not is_on_floor():
 		velocity.y += get_gravity().y * delta
 	
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y = -jump_speed
+	match _state:
+		State.CONTROL:
+			if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
+				velocity.x = 0
+			elif Input.is_action_pressed("left"):
+				velocity.x = -move_speed
+				_direction = -1
+			elif Input.is_action_pressed("right"):
+				velocity.x = move_speed
+				_direction = 1
+			else:
+				velocity.x = 0
+			
+			# TODO make these events
+			if is_on_floor() and Input.is_action_pressed("jump"):
+				velocity.y = -jump_speed
+			if is_on_floor() and Input.is_action_pressed("roll"):
+				_state = State.ROLL
+				$RollTimer.start()
+				collision_layer = 4
+		State.ROLL:
+			velocity.x = roll_speed * _direction
 		
 	move_and_slide()
 
@@ -28,7 +49,12 @@ func _input(event: InputEvent) -> void:
 		$Weapon.fire(position)
 
 func damage(amount: float) -> void:
+	print("Player hit")
 	$Health.take_damage(amount)
 
 func _on_health_died() -> void:
 	print("The player has died")
+
+func _on_roll_timer_timeout() -> void:
+	collision_layer = _init_layer
+	_state = State.CONTROL
