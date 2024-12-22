f22b469f8cd6e67e5a45750d45fddcf74a4cb288
Diego Frias <styx5242@gmail.com>
2024-12-21 16:27:01 -0800

    Fix massive player speed values
    We shouldn't be multiplying by `delta` when moving the player. Except
    when dealing with gravity (because gravity is in m^1s^-2).

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,13 +9,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
 		velocity.x = 0
 	elif Input.is_action_pressed("left"):
-		velocity.x = -move_speed * delta
+		velocity.x = -move_speed
 	elif Input.is_action_pressed("right"):
-		velocity.x = move_speed * delta
+		velocity.x = move_speed
 	else:
 		velocity.x = 0
-	
-	velocity.y += get_gravity().y * delta
+
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
 	
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		velocity.y = -jump_speed
