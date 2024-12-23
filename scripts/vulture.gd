c5a40ffd23b89fafd6338668286d0abf1b4c95d6
Diego Frias <styx5242@gmail.com>
2024-12-23 10:29:46 -0800

    Add vulture flapping

--- scripts/vulture.gd
+++ scripts/vulture.gd
@@ -1,19 +1,30 @@
 class_name Vulture extends Area2D
 
+# Movement
 @export var x_speed := 200.0
 @export var y_speed := 100.0
-@export var stop_dist := 150.0
+@export var y_acceleration := 400.0
+@export var x_acceleration := 300.0
+@export var stop_dist := 300.0
 @export var fly_height := 300.0
+# Attacking
 @export var attack_idle_time := 1.0
 @export var attack_windup_time := 0.3
 @export var attack_cooldown_time := 2.0
 @export var projectile: PackedScene
 @export var projectile_speed := 500.0
+# Flapping
+@export var flap_interval_time := 0.8
+@export var flap_y_speed_mean := 100.0
+@export var flap_y_speed_sd := 10.0
+@export var flap_x_speed_mean := 30.0
+@export var flap_x_speed_sd := 15.0
 
 var _state := State.IDLE
 var _tracking: Node2D
 var _idle_time := 0.0
 var _can_attack := true
+var _velocity := Vector2.ZERO
 const _GROUND_RAYCAST_DISTANCE = 10000.0
 
 enum State {
@@ -27,12 +38,16 @@ func _ready() -> void:
 	$DetectionZone.body_exited.connect(_on_detection_zone_body_exited)
 	$Health.damage_taken.connect(_on_health_damage_taken)
 	$Health.died.connect(_on_health_died)
+	_start_periodic_flap()
 
 func _process(delta: float) -> void:
 	var ground_y := _get_ground_y()
-	if abs((ground_y - fly_height) - position.y) > Constants.EPSILON:
-		position.y = move_toward(position.y, ground_y - fly_height, y_speed * delta)
-		_idle_time = 0.0
+	# This is the distance from the optimal idle position in the y axis
+	# this number will almost always be greater than zero
+	var fly_target := (ground_y - fly_height) - position.y
+	if fly_target > 1:
+		# Set our velocity to move us down (closer to fly target)
+		_velocity.y = move_toward(_velocity.y, y_speed, y_acceleration * delta)
 	
 	match _state:
 		State.IDLE:
@@ -40,21 +55,26 @@ func _process(delta: float) -> void:
 			pass
 		State.TRACKING:
 			assert(_tracking != null)
-			var x_dist = abs(position.x - _tracking.position.x)
-			if x_dist > stop_dist:
+			var x_dist := position.x - _tracking.position.x
+			if absf(x_dist) > stop_dist:
 				_idle_time = 0.0
-				position.x = move_toward(position.x, _tracking.position.x, x_speed * delta)
+				var target_speed := x_speed * -signf(x_dist)
+				# Velocity to point us towards our target
+				_velocity.x = move_toward(_velocity.x, target_speed, x_acceleration * delta)
 			else:
 				_idle_time += delta
-				if _idle_time > attack_idle_time and _can_attack:
+				if _idle_time > attack_idle_time and _can_attack and fly_target < 1:
 					_attack()
 		State.ATTACK_WINDUP:
 			pass
+	
+	position += _velocity * delta
 
 func _get_ground_y() -> float:
 	var space_state := get_world_2d().direct_space_state
 	var query := PhysicsRayQueryParameters2D.create(global_position, Vector2.DOWN * _GROUND_RAYCAST_DISTANCE, Constants.ENVIRONMENT_LAYER)
 	var result := space_state.intersect_ray(query)
+	# We _should_ always be over the ground
 	assert(!result.is_empty())
 	return result.get("position").y
 
@@ -74,6 +94,17 @@ func _attack() -> void:
 	await get_tree().create_timer(attack_cooldown_time).timeout
 	assert(not _can_attack)
 	_can_attack = true
+
+func _start_periodic_flap() -> void:
+	var flap_x_multiplier := 1.0
+	while true:
+		await get_tree().create_timer(flap_interval_time).timeout
+		_velocity.y = -randfn(flap_y_speed_mean, flap_y_speed_sd)
+		# We only want to flap sideways if we're idle
+		if _idle_time > 0:
+			_velocity.x = randfn(flap_x_speed_mean, flap_x_speed_sd) * flap_x_multiplier
+			# Alternates between 1 and -1 (flapping back and forth on the x-axis)
+			flap_x_multiplier *= -1.0
 
 func _on_detection_zone_body_entered(body: Node2D) -> void:
 	if body is Player:
