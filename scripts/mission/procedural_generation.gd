5f9186a7e674160c04469c923daaf0ad05cd9f72
Diego Frias <styx5242@gmail.com>
2024-12-26 19:58:35 -0800

    Use `FastNoiseLite` in procedural generation
    As opposed to using a sprite.

--- scripts/mission/procedural_generation.gd
+++ scripts/mission/procedural_generation.gd
@@ -3,19 +3,18 @@ class_name ProceduralGeneration extends TileMapLayer
 const BLOCK_ID = 1
 const BLOCK_TILE_ATLAS_POS = Vector2(0, 0)
 
-@export var noise_sprite: Sprite2D
 @export var x_distance = 200
 
-@onready var noise: Noise = noise_sprite.texture.noise
 var depth_multiplier = 20
 var y_distance = 50
+@onready var _noise: FastNoiseLite = FastNoiseLite.new()
 
 var _ground_heights: Array[int]
 
 func _ready() -> void:
-	noise.seed = randi_range(0, 1000)
+	_noise.seed = randi_range(0, 1000)
 	for x in x_distance:
-		var ground := absi(noise.get_noise_2d(x, 0) * depth_multiplier)
+		var ground := absi(_noise.get_noise_2d(x, 0) * depth_multiplier)
 		_ground_heights.append(ground)
 		for y in range(ground, y_distance):
 			set_cell(Vector2(x, y), BLOCK_ID, BLOCK_TILE_ATLAS_POS)
