188f2baadaec68f6cc2c857f923998b1eba322de
Tanner <tannerelephant@gmail.com>
2025-01-18 17:14:36 -0800

    adjust enemies and spawner to increase difficulty

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -13,21 +13,23 @@ func _process(delta: float) -> void:
 
 func spawn_enemies():
 	can_spawn = false
-	instantiate_enemies()
+	instantiate_enemies(3, 2)
 	await get_tree().create_timer(spawn_delay).timeout
 	can_spawn = true
 
-func instantiate_enemies():
-	var wolf = wolf_prefab.instantiate()
-	var random_index = randi_range(0, spawn_positions.size() - 1)
-	var spawn_position = spawn_positions[random_index].global_position
-	wolf.global_position = spawn_position
-	wolf._always_tracking = true
-	get_tree().root.add_child(wolf)
+func instantiate_enemies(wolf_amount: int, vulture_amount: int):
+	for i in range(wolf_amount):
+		var wolf = wolf_prefab.instantiate()
+		var random_index = randi_range(0, spawn_positions.size() - 1)
+		var spawn_position = spawn_positions[random_index].global_position
+		wolf.global_position = spawn_position
+		wolf._always_tracking = true
+		get_tree().root.add_child(wolf)
 	
-	var vulture = vulture_prefab.instantiate()
-	random_index = randi_range(0, spawn_positions.size() - 1)
-	spawn_position = spawn_positions[random_index].global_position
-	vulture.global_position = spawn_position
-	vulture._always_tracking = true
-	get_tree().root.add_child(vulture)
+	for i in range(vulture_amount):
+		var vulture = vulture_prefab.instantiate()
+		var random_index = randi_range(0, spawn_positions.size() - 1)
+		var spawn_position = spawn_positions[random_index].global_position
+		vulture.global_position = spawn_position
+		vulture._always_tracking = true
+		get_tree().root.add_child(vulture)
