533e1dec622dc6d22e5b6ad0e1405f765e933d65
Tanner <tannerelephant@gmail.com>
2025-01-18 17:29:21 -0800

    limit enemy amount in playtest

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var vulture_prefab: PackedScene
 @export var spawn_positions: Array[Node2D]
 @export var spawn_delay: float = 10
+@export var enemy_group: Node2D
 
 var can_spawn: bool = true
 
@@ -13,7 +14,8 @@ func _process(delta: float) -> void:
 
 func spawn_enemies():
 	can_spawn = false
-	instantiate_enemies(3, 2)
+	if enemy_group.get_child_count() <= 4:
+		instantiate_enemies(1, 1)
 	await get_tree().create_timer(spawn_delay).timeout
 	can_spawn = true
 
@@ -24,7 +26,7 @@ func instantiate_enemies(wolf_amount: int, vulture_amount: int):
 		var spawn_position = spawn_positions[random_index].global_position
 		wolf.global_position = spawn_position
 		wolf._always_tracking = true
-		get_tree().root.add_child(wolf)
+		enemy_group.add_child(wolf)
 	
 	for i in range(vulture_amount):
 		var vulture = vulture_prefab.instantiate()
@@ -32,4 +34,4 @@ func instantiate_enemies(wolf_amount: int, vulture_amount: int):
 		var spawn_position = spawn_positions[random_index].global_position
 		vulture.global_position = spawn_position
 		vulture._always_tracking = true
-		get_tree().root.add_child(vulture)
+		enemy_group.add_child(vulture)
