c841e748a9999355343423fcf3f2da6f2ee3acf4
Tanner <tannerelephant@gmail.com>
2025-02-13 16:30:07 -0800

    fix enemies being able to drop down from platforms above the platform the player is standing on

--- scripts/enemies/shooter.gd
+++ scripts/enemies/shooter.gd
@@ -51,7 +51,7 @@ func _physics_process(delta: float) -> void:
 			if Player.Instance.is_on_platform() and Player.Instance.get_platform_height() < global_position.y and is_on_floor() and _platform_detection.is_detecting_platform():
 				velocity.y = jump_height
 			
-			if !Player.Instance.is_on_platform() and Player.Instance.is_on_floor() and _platform_detection.is_on_platform():
+			if _platform_detection.is_on_platform() and (!Player.Instance.is_on_platform() or Player.Instance.get_platform_height() > _platform_detection.get_platform_height()) and Player.Instance.is_on_floor():
 				set_collision_mask_value(Math.ilog2(Constants.PLATFORM_LAYER) + 1, false)
 			else:
 				set_collision_mask_value(Math.ilog2(Constants.PLATFORM_LAYER) + 1, true)
