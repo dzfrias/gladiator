5cdb62dc19b26e08429c3c07576ca15ded0718d9
Diego Frias <styx5242@gmail.com>
2025-02-15 12:30:45 -0800

    Make idle shooter use `Weapon`

--- scripts/enemies/idle_shooter.gd
+++ scripts/enemies/idle_shooter.gd
@@ -1,27 +1,13 @@
 class_name IdleShooter extends CharacterBody2D
 
-@export_category("Shooting")
-@export var projectile: PackedScene
-@export var projectile_speed := 800.0
-@export var projectile_damage := 4.0
 @export var prepare_attack_time := 0.25
-@export var shoot_cooldown_avg: float = 1.4
-@export var shoot_cooldown_sd: float = 0.2
 @export var idle_time: float = 1
-@export var hide_time: float = 5
-@export var max_ammo: int = 3
-
 @export var impact_particle_prefab: PackedScene
 
 @onready var detection_zone = $DetectionZone
-@onready var firing_position = $FiringPosition
-
 var _state: State = State.HIDING
 @onready var _tracking: Node2D = Player.Instance
-var _can_attack: bool = true
 var _can_stand: bool = true
-var _direction
-var ammo: int
 
 enum State {
 	HIDING,
@@ -32,7 +18,6 @@ enum State {
 func _ready() -> void:
 	$Health.died.connect(_on_health_died)
 	$Health.damage_taken.connect(_on_health_damage_taken)
-	ammo = max_ammo
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -41,52 +26,32 @@ func _physics_process(delta: float) -> void:
 	match _state:
 		State.HIDING:
 			var xdist := _tracking.position.x - position.x
-			_direction = signf(xdist)
-			scale.x = _direction
+			$Direction.scalar = signf(xdist)
+			scale.x = $Direction.scalar
 			if detection_zone.has_overlapping_bodies() and _can_stand:
-				_stand()
+				_state = State.STANDING
 		State.STANDING:
 			scale.y = 1
 			var xdist := _tracking.position.x - position.x
-			_direction = signf(xdist)
-			scale.x = _direction
-			if ammo <= 0:
-				_hide()
+			$Direction.scalar = signf(xdist)
+			scale.x = $Direction.scalar
 			
-			if _can_attack:
-				_shoot(_direction)
+			_shoot()
 
-func _shoot(direction: float) -> void:
+func _shoot() -> void:
 	_state = State.SHOOTING
 	await get_tree().create_timer(prepare_attack_time).timeout
-	var p := projectile.instantiate() as HorizontalProjectile
-	get_tree().root.add_child(p)
-	var angle: float
-	if direction == 1:
-		angle = 0.0
-	else:
-		angle = PI
-	p.fire(projectile_speed, angle)
-	p.damage = projectile_damage
-	p.global_position = firing_position.global_position
-	ammo -= 1
-	await get_tree().create_timer(idle_time).timeout
-	_state = State.STANDING
-	_can_attack = false
-	var wait := randfn(shoot_cooldown_avg, shoot_cooldown_sd)
-	await get_tree().create_timer(wait).timeout
-	_can_attack = true
-
-func _stand():
-	_state = State.STANDING
+	while $Weapon.ammo > 0:
+		await $Weapon.fire($Direction)
+	
+	_hide()
 
 func _hide():
 	_state = State.HIDING
 	_can_stand = false
 	scale.y = 0.5
-	await get_tree().create_timer(hide_time).timeout
+	await $Weapon.reload()
 	_can_stand = true
-	ammo = max_ammo
 
 func _on_health_damage_taken(_amount: float, _direction: Vector2) -> void:
 	var impact_particles = impact_particle_prefab.instantiate()
