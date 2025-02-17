36c9169b98ab7957fb6f986a1d8c9c9ba50683bd
Diego Frias <styx5242@gmail.com>
2025-02-17 11:32:32 -0800

    Fix player platform raycast

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -215,20 +215,10 @@ func get_weapon() -> Weapon:
 	return _weapon
 
 func is_on_platform():
-	var result = _platform_raycast()
-	return !result.is_empty()
+	return $PlatformRaycast.is_colliding()
 
 func get_platform_height():
-	# Assums that platform is checked already
-	var result = _platform_raycast()
-	return result.position.y
-
-func _platform_raycast() -> Dictionary:
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(0, 100))
-	query.set_collision_mask(Constants.PLATFORM_LAYER)
-	var result = space_state.intersect_ray(query)
-	return result
+	return $PlatformRaycast.get_collision_point().y
 
 func _flash_invincible() -> void:
 	while collision_layer == Constants.INVINCIBLE_LAYER:
