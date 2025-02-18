425d39d15449d03baba751e0ef1355a2c465dff9
Diego Frias <styx5242@gmail.com>
2025-02-18 08:36:28 -0800

    Fix enemy platform detection
    This also removes the `platform_detection.tscn` scene, so we can easily
    modify the collision boxes/raycasts for a platform detector node.

--- scripts/platform_detection.gd
+++ scripts/platform_detection.gd
@@ -1,20 +1,10 @@
 extends Area2D
 
 func is_on_platform():
-	var result = _platform_raycast()
-	return !result.is_empty()
+	return $RayCast2D.is_colliding()
 
 func get_platform_height():
-	# Assums that platform is checked already
-	var result = _platform_raycast()
-	return result.position.y
+	return $RayCast2D.get_collision_point().y
 
 func is_detecting_platform():
 	return has_overlapping_bodies()
-
-func _platform_raycast() -> Dictionary:
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(0, 100))
-	query.set_collision_mask(Constants.PLATFORM_LAYER)
-	var result = space_state.intersect_ray(query)
-	return result
