ef4a459e5920a583e35fd4fc1386eaa25e330ce9
Tanner <tannerelephant@gmail.com>
2025-02-17 19:50:56 -0800

    adjust combat stats

--- scripts/enemies/shooter.gd
+++ scripts/enemies/shooter.gd
@@ -1,11 +1,5 @@
 class_name Shooter extends FollowEnemy
 
-@export_category("Movement")
-@export var y_cutoff: float = 100.0
-@export var y_axis_follow_time: float = 1.0
-
-var _player_follow_time: float = 0.0
-
 func _attack() -> void:
 	_state = State.ATTACKING
 	while $Weapon.ammo > 0:
