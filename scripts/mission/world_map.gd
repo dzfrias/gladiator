ff618c289c3199b843635002228afec7f54699e0
Tanner <tannerelephant@gmail.com>
2025-03-02 10:06:12 -0800

    expand encounters and increase chances of more complex encounters

--- scripts/mission/world_map.gd
+++ scripts/mission/world_map.gd
@@ -26,10 +26,10 @@ var _terrain := [
 	WeightedScene.new("res://scenes/modules/terrain/slope_down.tscn", 0.25),
 ]
 var _encounters := [
-	WeightedScene.new("res://scenes/modules/encounters/basic.tscn", 0.25),
-	WeightedScene.new("res://scenes/modules/encounters/encounter1.tscn", 0.25),
-	WeightedScene.new("res://scenes/modules/encounters/encounter2.tscn", 0.25),
-	WeightedScene.new("res://scenes/modules/encounters/encounter3.tscn", 0.25),
+	WeightedScene.new("res://scenes/modules/encounters/basic.tscn", 0.1),
+	WeightedScene.new("res://scenes/modules/encounters/encounter1.tscn", 0.3),
+	WeightedScene.new("res://scenes/modules/encounters/encounter2.tscn", 0.3),
+	WeightedScene.new("res://scenes/modules/encounters/encounter3.tscn", 0.3),
 ]
 var _enemies := [
 	WeightedScene.new("res://scenes/wolf.tscn", 0.5),
