66cf41ff5de1ef07ab8f5940734bca782f68298f
Tanner <tannerelephant@gmail.com>
2025-03-03 20:29:41 -0800

    add throwable explosive

---
+++ scripts/gadgets/throwable.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var explosionTime = 3
+@export var damage = 3
+
+@onready var explosion_radius = $ExplosionRadius
+func _ready() -> void:
+	await get_tree().create_timer(explosionTime).timeout
+	_explode()
+
+func _explode():
+	for body in explosion_radius.get_overlapping_bodies():
+		for child in body.get_children():
+			if child is Health:
+				child.take_damage(damage, Vector2.ZERO)
+	queue_free()
