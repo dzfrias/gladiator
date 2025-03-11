48990d9ded4ff6db87f269329ca9242f77f93551
Tanner <tannerelephant@gmail.com>
2025-03-10 20:07:29 -0700

    fix sound effects stopping after entity death

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -2,7 +2,8 @@ class_name AudioManager extends Node
 
 static func play_sound(node: Node2D, stream: AudioStreamWAV):
 	var audio_player = AudioStreamPlayer2D.new()
-	node.add_child(audio_player)
+	node.get_tree().current_scene.add_child(audio_player)
+	audio_player.global_position = node.global_position
 	audio_player.stream = stream
 	audio_player.play()
 	await audio_player.finished
