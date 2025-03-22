2c64ce3a43a893bbe4a656d33ed1d084591e87f9
Diego Frias <styx5242@gmail.com>
2025-03-22 07:58:16 -0700

    Clean up Hub UI
    Also add shortcuts to quit the various menus.

--- scripts/ui/mission_board_screen.gd
+++ scripts/ui/mission_board_screen.gd
@@ -1,16 +1,17 @@
-class_name MissionBoardScreen extends Node2D
+class_name MissionBoardScreen extends CanvasLayer
 
 var _board_item_scene = preload("res://scenes/mission_board_item.tscn")
 
 func _ready() -> void:
-	$CanvasLayer/Return.pressed.connect(_on_quit_pressed)
+	$Return.pressed.connect(_on_quit_pressed)
 	var buckles := HubManager.world.shop.buckles
-	$CanvasLayer/BucklesLabel.text = "Buckles: " + str(buckles)
+	$BucklesLabel.text = "Buckles: " + str(buckles)
+	
 	for mission in HubManager.world.mission_board.missions:
 		var board_item := _board_item_scene.instantiate() as MissionBoardItem
 		board_item.set_to_mission(mission)
 		board_item.mission_entered.connect(_on_mission_entered)
-		$CanvasLayer/Missions.add_child(board_item)
+		$Missions.add_child(board_item)
 
 func _on_quit_pressed() -> void:
 	HubManager.return_to_world()
--- scripts/ui/shop_screen.gd
+++ scripts/ui/shop_screen.gd
@@ -1,15 +1,15 @@
-class_name ShopScreen extends Node2D
+class_name ShopScreen extends CanvasLayer
 
 var _shop: Shop
 var _screen = null
 var _shop_item_scene := preload("res://scenes/shop_item.tscn")
 
 func _ready() -> void:
-	$CanvasLayer/Return.pressed.connect(_on_quit_pressed)
-	$CanvasLayer/Selection/Weapons.pressed.connect(_open_details_screen.bind(ScreenKind.WEAPONS))
-	$CanvasLayer/Selection/Armor.pressed.connect(_open_details_screen.bind(ScreenKind.ARMOR))
-	$CanvasLayer/Selection/Gadgets.pressed.connect(_open_details_screen.bind(ScreenKind.GADGETS))
-	$CanvasLayer/ItemsContainer/BackButton.pressed.connect(_on_back_button_pressed)
+	$Return.pressed.connect(_on_quit_pressed)
+	$Selection/Weapons.pressed.connect(_open_details_screen.bind(ScreenKind.WEAPONS))
+	$Selection/Armor.pressed.connect(_open_details_screen.bind(ScreenKind.ARMOR))
+	$Selection/Gadgets.pressed.connect(_open_details_screen.bind(ScreenKind.GADGETS))
+	$ItemsContainer/BackButton.pressed.connect(_on_back_button_pressed)
 	_shop = HubManager.world.shop
 	_shop.buckles_changed.connect(_update_buckles_label)
 	_update_buckles_label()
@@ -24,15 +24,15 @@ func _on_quit_pressed() -> void:
 	HubManager.return_to_world()
 
 func _on_back_button_pressed() -> void:
-	$CanvasLayer/Selection.visible = true
-	$CanvasLayer/ItemsContainer.visible = false
-	for child in $CanvasLayer/ItemsContainer/Items.get_children():
-		$CanvasLayer/ItemsContainer/Items.remove_child(child)
+	$Selection.visible = true
+	$ItemsContainer.visible = false
+	for child in $ItemsContainer/Items.get_children():
+		$ItemsContainer/Items.remove_child(child)
 	_screen = null
 
 func _open_details_screen(kind: ScreenKind) -> void:
-	$CanvasLayer/Selection.visible = false
-	$CanvasLayer/ItemsContainer.visible = true
+	$Selection.visible = false
+	$ItemsContainer.visible = true
 	_screen = kind
 	match kind:
 		ScreenKind.WEAPONS:
@@ -42,7 +42,7 @@ func _open_details_screen(kind: ScreenKind) -> void:
 				item.find_child("PriceLabel").text = str(weapon.price)
 				var btn = item.find_child("BuyButton")
 				btn.pressed.connect(_on_weapon_buy.bind(btn, weapon))
-				$CanvasLayer/ItemsContainer/Items.add_child(item)
+				$ItemsContainer/Items.add_child(item)
 
 func _on_weapon_buy(btn: Button, weapon: Shop.WeaponItem) -> void:
 	_shop.buy_weapon(weapon)
@@ -50,4 +50,4 @@ func _on_weapon_buy(btn: Button, weapon: Shop.WeaponItem) -> void:
 		btn.disabled = true
 
 func _update_buckles_label() -> void:
-	$CanvasLayer/BucklesLabel.text = "Buckles: " + str(_shop.buckles)
+	$BucklesLabel.text = "Buckles: " + str(_shop.buckles)
