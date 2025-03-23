5dd96d20879190052a7377d2735206b8b20e31b8
Diego Frias <styx5242@gmail.com>
2025-03-23 07:58:39 -0700

    Change spawn-in behavior for encounters

--- scripts/combat_encounter.gd
+++ scripts/combat_encounter.gd
@@ -1,12 +1,14 @@
 class_name CombatEncounter extends Module
 
-@export var wave_delay: float = 6.0
-@export var spawn_delay_mean: float = 0.8
-@export var spawn_delay_sd: float = 0.1
-@export var boundary_tolerance := 400
+@export var boundary_tolerance: float = 400.0
+@export var spawn_during_wave_min: int = 4
+@export var spawn_during_wave_max: int = 7
+@export var spawn_in_cooldown: float = 3.5
+@export var spawn_in_delay_mean: float = 0.7
+@export var spawn_in_delay_sd: float = 0.1
 
 var _started := false
-var _finished_spawning := false
+var _spawn_during_wave: int
 var _left_body: StaticBody2D
 var _right_body: StaticBody2D
 var _enemies := [
@@ -39,9 +41,12 @@ func _ready() -> void:
 	add_child(_right_body)
 	_left_body.position.x = -boundary_tolerance
 	_right_body.position.x = right_boundary + boundary_tolerance
+	
+	_spawn_during_wave = randi_range(spawn_during_wave_min, spawn_during_wave_max)
+	_spawn_enemies.call_deferred()
 
 func _process(_delta: float) -> void:
-	if not _started or not _finished_spawning:
+	if not _started:
 		return
 	
 	# Check if all enemies have been killed
@@ -69,29 +74,58 @@ func _start_wave() -> void:
 	assert(not _started)
 	_started = true
 	_set_barriers_enabled(true)
-	await get_tree().create_timer(wave_delay).timeout
-	
-	_finished_spawning = false
+	_spawn_in()
+
+func _spawn_in() -> void:
+	assert(_started)
+	var spawn_points := $SpawnPoints.get_children()
+	while _spawn_during_wave > 0:
+		var enemies_left := get_tree().get_node_count_in_group(_group_name)
+		var alive_ratio := float(enemies_left) / float(spawn_points.size())
+		
+		var to_spawn := 0
+		if alive_ratio < 0.2:
+			to_spawn = 3
+		elif alive_ratio < 0.7:
+			to_spawn = 2
+		elif alive_ratio < 1.0:
+			to_spawn = 1
+		to_spawn = mini(to_spawn, _spawn_during_wave)
+		_spawn_during_wave -= to_spawn
+		assert(_spawn_during_wave >= 0)
+		
+		for _i in range(to_spawn):
+			var choice := randi_range(0, spawn_points.size() - 1)
+			_spawn(spawn_points[choice], false)
+			await get_tree().create_timer(randfn(spawn_in_delay_mean, spawn_in_delay_sd)).timeout
+		
+		await get_tree().create_timer(spawn_in_cooldown).timeout
+
+func _set_barriers_enabled(enabled: bool) -> void:
+	_left_body.get_child(0).disabled = !enabled
+	_right_body.get_child(0).disabled = !enabled
+
+func _spawn_enemies() -> void:
 	for child in $SpawnPoints.get_children():
 		var spawn_point := child as Node2D
-		
-		var enemy_scene: PackedScene
-		match child.get_meta("spawnpoint_type", "any"):
+		_spawn(spawn_point)
+
+func _spawn(spawn_point: Node2D, allow_meta: bool = true) -> void:
+	var enemy_scene: PackedScene
+	if allow_meta:
+		match spawn_point.get_meta("spawnpoint_type", "any"):
 			"idle_ranged":
 				enemy_scene = _idle_shooter
 			"any":
 				enemy_scene = WorldMap.weighted_choice(_enemies).scene
 			var type:
 				printerr("invalid spawnpoint type: %s" % type)
-				continue
-		
-		var enemy_instance := enemy_scene.instantiate() as Node2D
-		enemy_instance.position = to_global(spawn_point.position)
-		enemy_instance.add_to_group(_group_name) 
-		get_tree().current_scene.add_child(enemy_instance)
-		await get_tree().create_timer(randfn(spawn_delay_mean, spawn_delay_sd)).timeout
-	_finished_spawning = true
-
-func _set_barriers_enabled(enabled: bool) -> void:
-	_left_body.get_child(0).disabled = !enabled
-	_right_body.get_child(0).disabled = !enabled
+				return
+	else:
+		enemy_scene = WorldMap.weighted_choice(_enemies).scene
+	
+	var enemy_instance := enemy_scene.instantiate() as Node2D
+	enemy_instance.position = to_global(spawn_point.position)
+	enemy_instance.add_to_group(_group_name)
+	enemy_instance.add_to_group("enemy")
+	get_tree().current_scene.add_child(enemy_instance)
