cb5a137a54b66ef6b79f113ee3ba37cb80c26038
Tanner <tannerelephant@gmail.com>
2025-03-28 19:57:17 -0700

    add basic laser pistol image

--- scripts/ui/ammo_ui.gd
+++ scripts/ui/ammo_ui.gd
@@ -1,16 +1,17 @@
-extends RichTextLabel
+extends Control
 
 @onready var weapon = Player.Instance.weapon()
 
-var infinite_ammo_text = "Ammo:  [img=8,8]assets/infinity_sign.png[/img]"
+@export var infinite_ammo_image: TextureRect
+@export var ammo_text: RichTextLabel
 
 func _ready() -> void:
 	var item = Player.Instance.inventory().get_held_item()
 	if item == Player.WEAPON_INDICATOR:
 		if weapon.weapon_stats.max_ammo == -1:
-			text = infinite_ammo_text
+			show_infinite_ammo_image()
 		else:
-			text = "Ammo: " + str(weapon.ammo) + "/" + str(weapon.weapon_stats.max_ammo)
+			show_ammo_text()
 		weapon.on_ammo_changed.connect(_on_ammo_changed)
 	else:
 		hide()
@@ -22,9 +23,9 @@ func _on_item_switched(current_item):
 		if weapon:
 			weapon.on_ammo_changed.disconnect(_on_ammo_changed)
 		if weapon.weapon_stats.max_ammo == -1:
-			text = infinite_ammo_text
+			show_infinite_ammo_image()
 		else:
-			text = "Ammo: " + str(weapon.ammo) + "/" + str(weapon.weapon_stats.max_ammo)
+			show_ammo_text()
 		weapon.on_ammo_changed.connect(_on_ammo_changed)
 	else:
 		weapon.on_ammo_changed.disconnect(_on_ammo_changed)
@@ -32,6 +33,16 @@ func _on_item_switched(current_item):
 
 func _on_ammo_changed(ammo: int, max_ammo: int) -> void:
 	if weapon.weapon_stats.max_ammo == -1:
-		text = infinite_ammo_text
+		show_infinite_ammo_image()
 	else:
-		text = "Ammo: " + str(weapon.ammo) + "/" + str(weapon.weapon_stats.max_ammo)
+		show_ammo_text()
+
+func show_ammo_text():
+	ammo_text.show()
+	infinite_ammo_image.hide()
+	ammo_text.text = str(weapon.weapon_stats.max_ammo)
+
+func show_infinite_ammo_image():
+	ammo_text.hide()
+	infinite_ammo_image.show()
+	
