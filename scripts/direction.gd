2baf854124e82e6d14dac878761af66bd29debbb
Diego Frias <styx5242@gmail.com>
2025-03-29 21:12:11 -0400

    Clean up Direction script

--- scripts/direction.gd
+++ scripts/direction.gd
@@ -3,16 +3,10 @@ class_name Direction extends Node
 var is_right: bool = true
 var scalar: float:
 	get:
-		if is_right:
-			return 1.0
-		else:
-			return -1.0
+		return 1.0 if is_right else -1.0
 	set(new):
 		assert(absf(new) == 1)
-		if new == 1.0:
-			is_right = true
-		else:
-			is_right = false
+		is_right = new == 1.0
 
 func switch() -> void:
 	is_right = !is_right
