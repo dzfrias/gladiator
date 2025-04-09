aab687c83c338fc0c1923590fd326f071a858ff5
Diego Frias <styx5242@gmail.com>
2025-04-09 07:04:05 -0700

    Improve suispider
    Fix jumping mechanic, make it do only a little bit of damage to itself,
    and increase explosion radius.

--- scripts/enemies/suispider.gd
+++ scripts/enemies/suispider.gd
@@ -1,27 +1,28 @@
 class_name Suispider extends FollowEnemy
 
 @export var damage: float = 5.0
-@export var explode_time: float = 9.0
+@export var self_damage: float = 2.0
+@export var explode_time: float = 2.0
 @export var distraction_jump_velocity: Vector2 = Vector2(300, -500.0)
-@export var distraction_jump_time: float = 0.5
 
 var _started_timer: bool = false
 
 func _attack() -> void:
 	if !_started_timer:
-		_startLightningTimer()
+		_start_lightning_timer()
 	_state = State.ATTACKING
 	velocity.y = distraction_jump_velocity.y
 	velocity.x = $Direction.scalar * distraction_jump_velocity.x
-	await get_tree().create_timer(distraction_jump_time).timeout
+	await hit_floor
 	assert(_state == State.ATTACKING)
 	_state = State.TRACKING
 
-func _startLightningTimer() -> void:
+func _start_lightning_timer() -> void:
 	_started_timer = true
-	await get_tree().create_timer(explode_time).timeout
-	if $ExplosionZone.has_overlapping_bodies():
+	while true:
+		await get_tree().create_timer(explode_time).timeout
 		for node in $ExplosionZone.get_overlapping_bodies():
 			var player := node as Player
-			player.damage(damage, Vector2.ZERO)	
-	$Health.take_damage(1000000, Vector2.ZERO)
+			player.damage(damage, Vector2.ZERO)
+		Debug.draw_circle(position, $ExplosionZone/CollisionShape2D.shape.radius)
+		$Health.take_damage(self_damage, Vector2.ZERO)
