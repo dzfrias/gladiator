4c723e08f8237e13d4558db80cdb32c9a1cf858c
Diego Frias <styx5242@gmail.com>
2025-04-15 16:31:38 -0700

    Fix explosion particles not appearing

--- scripts/free_on_finish.gd
+++ scripts/free_on_finish.gd
@@ -3,5 +3,7 @@ class_name FreeOnFinish extends CPUParticles2D
 @export var to_free: Node
 
 func _ready() -> void:
+	await get_tree().process_frame
+	emitting = true
 	await finished
 	to_free.queue_free()
