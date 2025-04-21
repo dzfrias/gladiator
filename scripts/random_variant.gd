d0ea6618bb8c25a2c18b6d20c8ba8643314b7473
Diego Frias <styx5242@gmail.com>
2025-04-20 20:30:16 -0700

    Prevent using the same decoration twice in a row

--- scripts/random_variant.gd
+++ scripts/random_variant.gd
@@ -3,6 +3,8 @@ extends Sprite2D
 @export var vertical: bool = false
 @export var restrict: Array[int] = []
 
+static var last_used: Dictionary
+
 func _ready() -> void:
 	assert(region_enabled)
 	
@@ -11,11 +13,14 @@ func _ready() -> void:
 	
 	assert(int(texture.get_size()[i]) % region_size[i] == 0)
 	
-	var p: int
-	if restrict.is_empty():
-		p = randi_range(0, int(texture.get_size()[i] / region_size[i]) - 1)
-	else:
-		p = restrict[randi_range(0, restrict.size() - 1)]
+	var p: int = -1
+	while p == -1 or p == last_used.get(texture.resource_path):
+		if restrict.is_empty():
+			p = randi_range(0, int(texture.get_size()[i] / region_size[i]) - 1)
+		else:
+			p = restrict[randi_range(0, restrict.size() - 1)]
+	assert(p != -1)
+	last_used[texture.resource_path] = p
 	
 	region_rect = Rect2(0, 0, region_size.x, region_size.y)
 	region_rect.position[i] = p * region_size[i]
