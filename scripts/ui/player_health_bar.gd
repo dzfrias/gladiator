71ca5637d80436f6a1f17eecd965371708dab647
Tanner <tannerelephant@gmail.com>
2025-04-25 08:05:29 -0700

    update player health ui
    Made player health bar more visible by moving it with the rest of the player ui.

---
+++ scripts/ui/player_health_bar.gd
@@ -0,0 +1,24 @@
+extends ProgressBar
+
+var health
+@export var tween_duration: float = 0.05
+
+func _ready() -> void:
+	health = Player.Instance.get_health()
+	max_value = health.max_health
+	health.damage_taken.connect(_on_health_lost)
+	health.health_gained.connect(_on_health_gained)
+
+func set_health(obj: Health):
+	health = obj
+
+func _on_health_lost(_amount: float, _direction: Vector2):
+	var tween := get_tree().create_tween()
+	tween.tween_property(self, "value", health.health, tween_duration)
+	tween.set_ease(Tween.EaseType.EASE_IN)
+
+func _on_health_gained(_amount: float):
+	var tween := get_tree().create_tween()
+	tween.tween_property(self, "value", health.health, tween_duration)
+	tween.set_ease(Tween.EaseType.EASE_IN)
+	await tween.finished
