d2d3c307c1afebf45ddd8d1a6cc42604a9313c38
Diego Frias <styx5242@gmail.com>
2025-04-26 15:59:09 -0700

    Improve tutorial
    This includes adding some terrain variety, teaching the player how to
    jump, and the spider valley. Also update UIDs as necessary.

---
+++ scripts/spider_valley.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var text_wait_time: float = 1.0
+@export var enemy_to_spawn: PackedScene
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	if body is not Player:
+		return
+	
+	_start.call_deferred()
+
+func _start() -> void:
+	monitoring = false
+	_make_text_appear()
+	for _i in range(50):
+		var spawn_point = $Spawn1 if randf() <= 0.5 else $Spawn2
+		var enemy := enemy_to_spawn.instantiate() as Node2D
+		get_tree().current_scene.add_child(enemy)
+		enemy.notify(0)
+		enemy.global_position = spawn_point.global_position
+		await get_tree().create_timer(0.05).timeout
+
+func _make_text_appear() -> void:
+	await get_tree().create_timer(text_wait_time).timeout
+	$Text.visible = true
+	$Text2.visible = true
