83b218ded0048142efa5f6d7d65df8dc9d79ccc1
Diego Frias <styx5242@gmail.com>
2025-04-28 19:41:06 -0700

    Add particle effects to underground stun

--- scripts/passives/underground_stun.gd
+++ scripts/passives/underground_stun.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 func _on_exited_underground() -> void:
 	if Player.Instance.velocity.y == 0:
 		return
+	$Particles.emitting = true
 	for body in get_overlapping_bodies():
 		if body is Enemy:
 			body.set_stunned(stun_duration)
