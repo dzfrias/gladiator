e52960c2a37caddb8e08b561eab78ea51e468af2
Tanner <tannerelephant@gmail.com>
2025-05-06 20:18:23 -0700

    fix jump sound effect

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -2,12 +2,13 @@ extends Node
 
 var music: AudioStreamPlayer
 
-func play_sound(node: Node, stream: AudioStreamWAV):
+func play_sound(node: Node, stream: AudioStreamWAV, volume_db = 0):
 	var audio_player = AudioStreamPlayer2D.new()
 	node.get_tree().current_scene.add_child(audio_player)
 	if node is Node2D:
 		audio_player.global_position = node.global_position
 	audio_player.stream = stream
+	audio_player.volume_db = volume_db
 	audio_player.play()
 	await audio_player.finished
 	audio_player.queue_free()
