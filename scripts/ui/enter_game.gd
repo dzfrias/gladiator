98a59be923b1cb6ad45968f88890953162b578dc
Diego Frias <styx5242@gmail.com>
2025-05-09 22:36:18 -0700

    Make buttons hover

--- scripts/ui/enter_game.gd
+++ scripts/ui/enter_game.gd
@@ -1,17 +1,7 @@
 extends Button
 
-@export var hover_offset: Vector2 = Vector2(0, -10)
-
-@onready var _original_pos = position
-
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
 	HubManager.go_to_hub()
-
-func _process(_delta: float) -> void:
-	if is_hovered():
-		position = _original_pos + hover_offset
-	else:
-		position = _original_pos
--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -3,7 +3,8 @@ class_name Menu extends Node
 func _ready() -> void:
 	var buttons = get_all_buttons(self)
 	for button in buttons:
-		button.mouse_entered.connect(_on_button_hovered)
+		button.mouse_entered.connect(_on_button_hovered.bind(button))
+		button.mouse_exited.connect(_on_button_unhovered.bind(button))
 		button.pressed.connect(_on_button_pressed)
 	
 	get_tree().node_added.connect(_on_node_added)
@@ -16,13 +17,18 @@ func get_all_buttons(node: Node) -> Array[Button]:
 		buttons += get_all_buttons(child)
 	return buttons
 
-func _on_button_hovered() -> void:
+func _on_button_hovered(button: Button) -> void:
+	button.position.y -= 10
 	AudioManager.play_ui_sound(get_tree().current_scene, load("res://assets/SoundEffects/ui_select_1.wav"), -15)
+
+func _on_button_unhovered(button: Button) -> void:
+	button.position.y += 10
 
 func _on_button_pressed() -> void:
 	AudioManager.play_ui_sound(get_tree().current_scene, load("res://assets/SoundEffects/ui_select_2.wav"), -15)
 
 func _on_node_added(node: Node) -> void:
 	if node is Button and is_ancestor_of(node):
-		node.mouse_entered.connect(_on_button_hovered)
+		node.mouse_entered.connect(_on_button_hovered.bind(node))
+		node.mouse_exited.connect(_on_button_unhovered.bind(node))
 		node.pressed.connect(_on_button_pressed)
