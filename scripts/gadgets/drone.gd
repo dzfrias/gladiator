e63ebdb0c9e653be2fc2d816e5f1650e9af9ecad
Tanner <tannerelephant@gmail.com>
2025-05-10 09:09:08 -0700

    add hover tween to drone

--- scripts/gadgets/drone.gd
+++ scripts/gadgets/drone.gd
@@ -1,11 +1,15 @@
 class_name Drone extends Node2D
 
 @export var active_time = 5
+var move_dist = Vector2(0, 15)
+var tween_duration = 1
+var y_move_dir = 1
 
 func init(direction: Direction):
 	pass
 
 func _ready() -> void:
+	_hover()
 	await get_tree().create_timer(active_time).timeout
 	queue_free()
 
@@ -14,6 +18,14 @@ func _process(delta: float) -> void:
 	if closest_target and $Weapon.can_fire:
 		var angle = global_position.angle_to_point(closest_target.global_position)
 		$Weapon.fire(angle)
+
+func _hover():
+	while true:
+		var tween := get_tree().create_tween()
+		tween.tween_property(self, "global_position", global_position + move_dist * y_move_dir, tween_duration)
+		tween.set_ease(Tween.EaseType.EASE_IN)
+		await tween.finished
+		y_move_dir *= -1
 
 func find_closest_target():
 	var closest_dist = INF
